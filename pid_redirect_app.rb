a0fef3484a88179b79e84d996bfa326f2de40f25
scollett <stephanie.collett@ucop.edu>
2013-06-19 13:14:13 -0700

    Create PID redirect application

---
+++ pid_redirect_app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require './lib/shortcake/shortcake'
+
+redis = Redis.new
+shorty = Shortcake.new('short', {:host => "localhost", :port => 6379})
+CODE_AND_PARTIAL = Regexp.compile(/^(\/[0-9]{1,12})(.*)/)
+
+get %r{/([0-9]{1,12}/.+)} do
+  shorty.create_or_update('1234', 'http://google.com')
+  shortcode, code, partial = request.fullpath.match(CODE_AND_PARTIAL).to_a 
+  url = shorty.get(code[1,code.length])
+  url.nil? ? 404 : redirect(url + partial.to_s)
+end
+
+get '/:shortcode' do
+  shorty.create_or_update('1234', 'http://google.com')
+  url = shorty.get(params[:shortcode])
+  url.nil? ? 404 : redirect(url)
+end
