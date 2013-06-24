d7f8ad15bbcae35f54d41d63066ab44ad521593b
scollett <stephanie.collett@ucop.edu>
2013-06-24 13:03:21 -0700

    Fix tests by using PID model, not shortcake directly

--- test/integration/test_redirect_controller.rb
+++ test/integration/test_redirect_controller.rb
@@ -8,19 +8,22 @@ class PidRedirectApp < Test::Unit::TestCase
   end
   
   def setup
-    @shorty = Shortcake.new('short', {:host => "localhost", :port => 6379})
-    @shorty.flushall!
+    Pid.flush!
+    @group = Group.new(:id => 'UCLA', :name => 'test_group')
+    @user = User.new(:handle => 'test_user', :name => 'Test User')
+    @group.users << @user
+    @group.save
   end
   
   def test_pid_redirect
-    @shorty.create_or_update('1', 'http://google.com')
+    Pid.mint(:url => 'http://google.com', :username => @user.handle, :change_category => 'User_Entered')
     get '/1'
     assert_equal 'http://google.com', last_response.location
     assert_equal 302, last_response.status
   end
   
   def test_pid_partial_redirect
-    @shorty.create_or_update('1', 'http://google.com')
+    Pid.mint(:url => 'http://google.com', :username => @user.handle, :change_category => 'User_Entered')
     get '/1/search?q=elmo'
     assert_equal 'http://google.com/search?q=elmo', last_response.location
     assert_equal 302, last_response.status
